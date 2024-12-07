a3a3e940902909d6b2f76d6156fa5bd0364f18bc
polar-solvent <150511665+polar-solvent@users.noreply.github.com>
2024-12-07 17:09:55 +0900

    最初のプロトタイプ

---
+++ main.py
@@ -0,0 +1,189 @@
+import sys
+from making import main
+from PyQt6.QtWidgets import QApplication,QWidget,QLineEdit,QMessageBox,QSpinBox,QDialog,QComboBox,QScrollArea,QSizePolicy,QHBoxLayout,QVBoxLayout,QPushButton,QCheckBox,QFileDialog,QLabel,QInputDialog
+from PyQt6.QtCore import Qt
+
+class Widget(QWidget):
+    def __init__(self):
+        self.input_path = []
+        self.upright = False
+        self.reverse = False
+        self.arrange = 0
+        self.img_width = 0
+        self.img_height = 0
+        self.interval = 1
+        self.dest = "./assets/dest"
+        self.name = "frame"
+
+        super().__init__()
+        
+        self.setStyleSheet('font-family: "Noto Sans CJK JP"; font-size: 22px')
+        self.setGeometry(10,10,400,225)
+        self.setWindowTitle("QScrollAreaは解決")
+
+        vbl = QVBoxLayout()
+        self.setLayout(vbl)
+
+        hbl1 = QHBoxLayout()
+        vbl.addLayout(hbl1)
+
+        self.boxup = QCheckBox("upright", self)
+        hbl1.addWidget(self.boxup)
+        self.boxup.toggled.connect(self.change)
+
+        self.boxre = QCheckBox("reverse", self)
+        hbl1.addWidget(self.boxre)
+        self.boxre.toggled.connect(self.change)
+
+        self.comboboxar = QComboBox()
+        hbl1.addWidget(self.comboboxar)
+        self.arranges = ["上ぞろえ", "中央ぞろえ", "下ぞろえ"]
+        for line in self.arranges:
+            self.comboboxar.addItem(line)
+        self.combotest = QLabel("")
+        self.comboboxar.currentTextChanged.connect(self.change)
+        self.comboboxar.setCurrentIndex(0)
+        hbl1.addWidget(self.combotest)
+
+        self.sizes = QLabel(f"横幅:{self.img_width}, 縦幅:{self.img_height}, 間隔:{self.interval}")
+        hbl1.addWidget(self.sizes)
+        self.size_button = QPushButton("サイズ変更", self)
+        self.size_button.clicked.connect(self.insert_number)
+        hbl1.addWidget(self.size_button)
+
+
+        hbl2 = QHBoxLayout()
+        vbl.addLayout(hbl2)
+
+        self.filearea = QScrollArea(self)
+        self.filename = QLabel("ファイル名", self.filearea)
+        self.filearea.setWidget(self.filename)
+        hbl2.addWidget(self.filearea)
+
+        button_file = QPushButton("画像選択",self)
+        button_file.clicked.connect(self.file)
+        hbl2.addWidget(button_file)
+
+        hbl3 = QHBoxLayout()
+        vbl.addLayout(hbl3)
+
+        self.destarea = QScrollArea(self)
+        self.destname = QLabel(str(self.dest), self.destarea)
+        self.destarea.setWidget(self.destname)
+        hbl3.addWidget(self.destarea)
+
+        button_dest = QPushButton("保存ディレクトリ",self)
+        button_dest.clicked.connect(self.choose_dest)
+        hbl3.addWidget(button_dest)
+
+        hbl4 = QHBoxLayout()
+        vbl.addLayout(hbl4)
+
+        name_string = QLabel("保存名:")
+        self.insert_name = QLineEdit("frame",self)
+        self.insert_name.textEdited.connect(self.name_pushed)
+        hbl4.addWidget(name_string)
+        hbl4.addWidget(self.insert_name)
+
+        button_run = QPushButton("生成",self)
+        button_run.clicked.connect(self.run)
+        hbl4.addWidget(button_run)
+
+    def change(self):
+        self.upright = self.boxup.isChecked()
+        self.reverse = self.boxre.isChecked()
+        self.arrange = self.comboboxar.currentIndex()
+
+    
+
+    def insert_number(self):
+        self.inum_d = QDialog()
+        self.inum_d.setWindowTitle("サイズの入力")
+        self.inum_d.setStyleSheet('font-family: "Noto Sans CJK JP"; font-size: 22px')
+        self.inum_d.setWindowModality(Qt.WindowModality.ApplicationModal)
+        self.spinwi, self.spinhe, self.spinin = QSpinBox(), QSpinBox(), QSpinBox()
+        for spin in (self.spinwi, self.spinin, self.spinhe):
+            spin.setRange(0,0xFFFF)
+            spin.setSingleStep(1)
+        self.spinin.setRange(1,0xFFFF)
+        self.spinwi.setValue(self.img_width)
+        self.spinhe.setValue(self.img_height)
+        self.spinin.setValue(self.interval)
+        
+        self.labelwi = QLabel(f"縦幅:{self.img_width} → ")
+        self.labelhe  = QLabel(f"横幅:{self.img_height} → ")
+        self.labelin = QLabel(f"間隔:{self.interval} → ")
+
+        vbli = QVBoxLayout()
+        self.inum_d.setLayout(vbli)
+        hbli1 = QHBoxLayout()
+        vbli.addLayout(hbli1)
+
+        hbli1.addWidget(self.labelwi)
+        hbli1.addWidget(self.spinwi)
+
+        hbli2 = QHBoxLayout()
+        vbli.addLayout(hbli2)
+
+        hbli2.addWidget(self.labelhe)
+        hbli2.addWidget(self.spinhe)
+
+        hbli3 = QHBoxLayout()
+        vbli.addLayout(hbli3)
+
+        hbli3.addWidget(self.labelin)
+        hbli3.addWidget(self.spinin)
+
+        self.insert_button = QPushButton("入力完了",self.inum_d)
+        self.insert_button.clicked.connect(self.check_wi)
+        hbli4 = QHBoxLayout()
+        vbli.addLayout(hbli4)
+        hbli4.addWidget(self.insert_button)
+
+        self.inum_d.exec()
+
+    def check_wi(self):
+        if self.spinwi.value()==0 or self.spinin.value() <= self.spinwi.value():
+            self.img_width = self.spinwi.value()
+            self.img_height = self.spinhe.value()
+            self.interval = self.spinin.value()
+            self.inum_d.close()
+            self.sizes.setText(f"横幅:{self.img_width}, 縦幅:{self.img_height}, 間隔:{self.interval}")
+        else:
+            QMessageBox.critical(self.inum_d,"","間隔が横幅を超過しています！")
+            self.spinin.setValue(self.spinwi.value())
+        
+
+    def file(self):
+        name,ok = QFileDialog.getOpenFileNames(self)
+        if(ok):
+            self.input_path = name
+            self.filename = QLabel(", ".join(name), self.filearea)
+            self.filearea.setWidget(self.filename)
+
+    def choose_dest(self):
+        name = QFileDialog.getExistingDirectory(self,"保存ディレクトリ")
+        self.dest = name
+        self.destname = QLabel(str(self.dest), self.destarea)
+        self.destarea.setWidget(self.destname)
+        print(self.dest)
+
+    def name_pushed(self):
+        self.name = self.insert_name.text()
+
+    def run(self):
+        main(self.input_path,self.upright,self.reverse,self.arrange,self.img_width,self.img_height,self.interval,self.dest,self.name)
+        QMessageBox.information(self,"","生成が終了しました！")
+        # main(self.input_path, self.upright, self.reverse, self.arrange, self.width, self.height, self.interval, self.dest, self.name)
+            
+
+        # title,ok = QInputDialog.getText(self, "input window", "Input title of window.")
+        # if(ok and title!=""):
+        #     self.setWindowTitle(title)
+        # else:
+        #     self.windowTitle("ウィンドウ")
+
+qAp = QApplication(sys.argv)
+wid = Widget()
+wid.show()
+qAp.exec()
---
+++ making.py
@@ -0,0 +1,163 @@
+import argparse
+import cv2
+import numpy as np
+import sys
+import os
+import re
+
+# parser = argparse.ArgumentParser(description="指定した画像ファイルを任意のpxずつずらし、bmp画像にして出力する。")
+# parser.add_argument("input_path", nargs="+", type=str, help="指定する画像のパス。複数のパスを入力可")
+# parser.add_argument("-u","--upright", action="store_true", help="指定した画像をずらす方向を定める。デフォルトは横向き、オプションを入れると縦向きになる。")
+# parser.add_argument("-r","--reverse", action="store_true", help="オプションを入れると指定した画像をずらす向きを逆転させる。")
+# parser.add_argument("-a","--arrange", type=int, choices=range(3), default=0, help="複数の画像を読み込むとき、上ぞろえ(0)・中央ぞろえ(1)・下ぞろえ(2)を選択できる。デフォルトは上ぞろえ(0)")
+# parser.add_argument("-W","--width", type=int, default=None, help="出力する画像の幅。デフォルトは入力した画像の横幅の和")
+# parser.add_argument("-H","--height", type=int, default=None, help="出力する画像の高さ。デフォルトは入力した画像の高さのうち最大のもの")
+# parser.add_argument("-I","--interval", type=int, default=1, help="出力する画像1枚ごとにずらすpx数。デフォルトは1px")
+# parser.add_argument("-d","--dest", type=str, default="./assets/dest", help="出力先のディレクトリ(指定したディレクトリが存在しない場合は作成される)。デフォルトはカレントディレクトリ内のassets/dest")
+# parser.add_argument("-n","--name", type=str, default="frame", help="出力する画像の名前。デフォルトはframe_数字.bmpで、frameの部分を変えられる。")
+# args = parser.parse_args()
+
+# # parserのtype=boolはだめらしいのでなんとかした(-u, -r)
+
+def main(input_path, upright=False, reverse=False, arrange=0, width=None, height=None, interval=1, dest="./assets/dest", name="frame"):
+    print("1")
+    # if width is not None and width < 1:
+    #     print("enter width more than 0")
+    #     sys.exit(1)
+
+    # if height is not None and height < 1:
+    #     print("enter height more than 0")
+    #     sys.exit(1)
+
+    # if interval < 1:
+    #     print("enter interval more than 0")
+    #     sys.exit(1)
+
+    if not re.fullmatch(r"[-\w]+", name):
+        print("enter valid file name")
+        sys.exit(1)
+
+    import_imgs = [cv2.imread(i) for i in input_path]
+    if any(e is None for e in import_imgs):
+        print('not correct path of image')
+        sys.exit(1)
+
+    img_shape = np.array([i.shape for i in import_imgs]) #動いた。縦だけデータ取る
+    h,w,c = img_shape[:,0], img_shape[:,1], img_shape[:,2]
+
+    # try :
+    #     os.makedirs(dest, exist_ok=True)
+    # except Exception as e:
+    #     print(e)
+    #     sys.exit(1)
+
+    print("2")
+
+    if upright:
+        if width == 0:
+            frame_height = max(w)
+        else:
+            frame_height = width
+        if height == 0:
+            frame_width = sum(h)
+        else:
+            frame_width = height
+        if interval > frame_width:
+            print("enter interval less than width")
+            sys.exit(1)
+        img = np.zeros((2*frame_width+sum(h),max(w),3),dtype=np.uint8)
+        if arrange == 0:
+            for i, image in enumerate(import_imgs):
+                img[frame_width+sum(h[:i]):frame_width+sum(h[:i+1]), :w[i], :] = image
+            if max(w) >= frame_height:
+                img = img[:,:frame_height,:]
+            else:
+                img = np.hstack([img, np.zeros((2*frame_width+sum(h),frame_height-max(w),3),dtype=np.uint8)])
+        elif arrange == 1:
+            for i, image in enumerate(import_imgs):
+                m = int((max(w)-w[i])//2)
+                if max(w)%2 == 0 and w[i]%2 == 1:
+                    m+=1
+                img[frame_width+sum(h[:i]):frame_width+sum(h[:i+1]), m:m+w[i], :] = image
+            if max(w) >= frame_height:
+                margin = int((max(w)-frame_height)//2)
+                if max(w)%2 == 0 and frame_height%2 == 1:
+                    margin+=1
+                img = img[:,margin:margin+frame_height,:]
+            else:
+                margin = int((frame_height-max(w))//2)
+                if frame_height%2 == 0 and max(w)%2 == 1:
+                    margin+=1
+                img = np.hstack([np.zeros((2*frame_width+sum(h),margin,3),dtype=np.uint8),img,np.zeros((2*frame_width+sum(h),frame_height-margin-max(w),3),dtype=np.uint8)])
+        elif arrange == 2:
+            for i, image in enumerate(import_imgs):
+                img[frame_width+sum(h[:i]):frame_width+sum(h[:i+1]), max(w)-w[i]:, :] = image
+            if max(w) >= frame_height:
+                img = img[:,max(w)-frame_height:,:]
+            else:
+                img = np.hstack([np.zeros((2*frame_width+sum(h),frame_height-max(w),3),dtype=np.uint8), img])
+        start, i = 0, 0
+        if reverse: start, interval = sum(h)+frame_width-1, -interval
+        while start <= sum(h) + frame_width and start >= 0:
+            currentframe=img[start:start+frame_width,:,:]
+            cv2.imwrite(f"{dest}/{name}_{i}.bmp",currentframe)
+            start += interval
+            i += 1
+    else:
+        if height == 0:
+            frame_height = max(h)
+        else:
+            frame_height = height
+        if width == 0:
+            frame_width = sum(w)
+        else:
+            frame_width = width
+        if interval > frame_width:
+            print("enter interval less than width")
+            sys.exit(1)
+        print("3")
+        img = np.zeros((max(h),2*frame_width+sum(w),3),dtype=np.uint8)
+        if arrange == 0:
+            for i, image in enumerate(import_imgs):
+                img[:h[i], frame_width+sum(w[:i]):frame_width+sum(w[:i+1]), :] = image
+            if max(h) >= frame_height:
+                img = img[:frame_height,:,:]
+            else:
+                img = np.vstack([img, np.zeros((frame_height-max(h),2*frame_width+sum(w),3),dtype=np.uint8)])
+        elif arrange == 1:      #原因はおそらく//2を二回しているから--→102行目imgの縦を最初からframe_heightに設定するとできる？
+            for i, image in enumerate(import_imgs): #↑貼り付ける側と貼り付けられる側の偶奇で場合分けして解決
+                m = int((max(h)-h[i])//2)         #大=小または大：奇、小：偶のとき
+                if max(h)%2 == 0 and h[i]%2 == 1: #大：偶、小：奇のとき
+                    m+=1
+                img[m:m+h[i], frame_width+sum(w[:i]):frame_width+sum(w[:i+1]), :] = image
+            if max(h) >= frame_height:
+                margin = int((max(h)-frame_height)//2)
+                if max(h)%2 == 0 and frame_height&2 == 1:
+                    margin+=1
+                img = img[margin:margin+frame_height,:,:]
+            else:
+                margin = int((frame_height-max(h))//2)
+                if frame_height%2 == 0 and max(h)%2 == 1:
+                    margin+=1
+                img = np.vstack([np.zeros((margin,2*frame_width+sum(w),3),dtype=np.uint8),img,np.zeros((frame_height-margin-max(h),2*frame_width+sum(w),3),dtype=np.uint8)])
+        elif arrange == 2:
+            for i, image in enumerate(import_imgs):
+                img[max(h)-h[i]:, frame_width+sum(w[:i]):frame_width+sum(w[:i+1]), :] = image
+            if max(h) >= frame_height:
+                img = img[max(h)-frame_height:,:,:]
+            else:
+                img = np.vstack([np.zeros((frame_height-max(h),2*frame_width+sum(w),3),dtype=np.uint8), img])
+        print("4")
+        start, i = 0, 0
+        if reverse: start, interval = sum(w)+frame_width-1, -interval
+        while start <= sum(w) + frame_width and start >= 0:
+            currentframe=img[:,start:start+frame_width,:]
+            cv2.imwrite(f"{dest}/{name}_{i}.bmp",currentframe)
+            start += interval
+            i += 1
+        print("5")
+        print(f"{dest}/{name}_.bmp")
+
+if __name__ == "__main__":
+    main()
+    
